7440ac66cc95230463a69d907009f1a30d7a8c9c
Lee-bumgi <bumgiv@naver.com>
2022-08-02 19:24:02 +0900

    wifi

--- 22.08.01/wifi/wifi.ino
+++ 22.08.01/wifi/wifi.ino
@@ -1,9 +1,12 @@
+#include <ArduinoJson.h>
+
 #include <WiFi.h>
 #include <HTTPClient.h>
+
 // wifi 아이디 비번
 const char* ssid = "AndroidHotspot0975";
 const char* password =  "the808bg";
-String address = "http://192.168.185.212:5050/led";
+String address = "https://api.openweathermap.org/data/2.5/weather?q=Gwangju&appid=e299ff2f9fb6bdd3df10fc9fe5fb163d&units=metric";
 String result = "";
 HTTPClient http;
 void setup() {
@@ -21,15 +24,29 @@ void loop() {
     http.begin(address);
     int httpCode = http.GET();
     if (httpCode > 0) {
-      Serial.println(httpCode);
+      //Serial.println(httpCode);
       result = http.getString();
-      Serial.println(result);
+      //Serial.println(result);
+      // Arduino 에서는 JSON형태의 데이터를 다루기 위해서는
+      // 특정 메모리 공간이 필요하다.
+      // 공간 만드는 법은 2가지 : 정적,동적
+      DynamicJsonBuffer jsonBuffer;
+      JsonObject& root = jsonBuffer.parseObject(result);
+      JsonObject& main = root["main"];
+      float temp = main["temp"];
+      Serial.print("현재 온도 : ");
+      Serial.print(temp);
+      Serial.println("도");
     }
-    if (result == "1"){
-      digitalWrite(18,1);
-    } else{
-      digitalWrite(18,0);
-    }
+  
+   
+
+    
+//    if (result == "1"){
+//      digitalWrite(18,1);
+//    } else{
+//      digitalWrite(18,0);
+//    }
     
     http.end();
   } else {
